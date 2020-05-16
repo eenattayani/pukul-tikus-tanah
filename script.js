b57f8f654cf8ebce7eb456973213fc2511af1371
eenattayani <enryfirmana@gmail.com>
2020-05-16 10:36:11 +0700

    tambah game

---
+++ script.js
@@ -0,0 +1,46 @@
+const tanah = document.querySelectorAll('.tanah');
+const tikus = document.querySelectorAll('.tikus');
+const papanSkor = document.querySelector('.papan-skor');
+const pop = document.querySelector('#pop');
+
+let tanahSebelumnya;
+let selesai = false;
+let skor = 0;
+
+function randomTanah(tanah) {
+    const tRandom = Math.floor(Math.random() * tanah.length);
+    return tRandom;
+}
+
+function munculkanTikus() {
+    const tanahKe = randomTanah(tanah);
+    tanah[tanahKe].classList.add('muncul');
+
+    setTimeout(() => {
+        tanah[tanahKe].classList.remove('muncul');
+        if(!selesai){
+            munculkanTikus();
+        }
+    }, 700);
+}
+
+function mulai() {
+    selesai = false;
+    skor = 0;
+    papanSkor.textContent = 0;
+    munculkanTikus();
+    setTimeout(() => {
+        selesai = true;
+    }, 10000);
+}
+
+function pukul() {
+    skor++;
+    papanSkor.textContent = skor;
+    pop.play();
+    this.parentNode.classList.remove('muncul');
+}
+
+tikus.forEach(t => {
+    t.addEventListener('click', pukul);  
+});
